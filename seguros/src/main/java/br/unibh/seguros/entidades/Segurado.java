8a1614b2df40ba04b9fe67c0784d353a515a3137
11423066 <11423066@ESTB7LI0309.unibh.edu>
2016-08-18 21:10:01 -0300

    Teste de Envio

--- seguros/src/main/java/br/unibh/seguros/entidades/Segurado.java
+++ seguros/src/main/java/br/unibh/seguros/entidades/Segurado.java
@@ -1,10 +1,96 @@
 package br.unibh.seguros.entidades;
 
-public class Segurado {
+import java.util.Date;
+
+public class Segurado extends Pessoa {
 
 	private String classe;
 	private String numeroRG;
 	private String orgaoExpedidorRG;
+	public String getClasse() {
+		return classe;
+	}
+	public void setClasse(String classe) {
+		this.classe = classe;
+	}
+	public String getNumeroRG() {
+		return numeroRG;
+	}
+	public void setNumeroRG(String numeroRG) {
+		this.numeroRG = numeroRG;
+	}
+	public String getOrgaoExpedidorRG() {
+		return orgaoExpedidorRG;
+	}
+	public void setOrgaoExpedidorRG(String orgaoExpedidorRG) {
+		this.orgaoExpedidorRG = orgaoExpedidorRG;
+	}
+	public String getNumeroHabilitacao() {
+		return numeroHabilitacao;
+	}
+	public void setNumeroHabilitacao(String numeroHabilitacao) {
+		this.numeroHabilitacao = numeroHabilitacao;
+	}
+	public String getTipoHabilitacao() {
+		return tipoHabilitacao;
+	}
+	public void setTipoHabilitacao(String tipoHabilitacao) {
+		this.tipoHabilitacao = tipoHabilitacao;
+	}
+	public Date getDataValidadeHabilitacao() {
+		return dataValidadeHabilitacao;
+	}
+	public void setDataValidadeHabilitacao(Date dataValidadeHabilitacao) {
+		this.dataValidadeHabilitacao = dataValidadeHabilitacao;
+	}
+	public Date getDataPrimeiraHabilitacao() {
+		return dataPrimeiraHabilitacao;
+	}
+	public void setDataPrimeiraHabilitacao(Date dataPrimeiraHabilitacao) {
+		this.dataPrimeiraHabilitacao = dataPrimeiraHabilitacao;
+	}
+	public String getLogradouro() {
+		return logradouro;
+	}
+	public void setLogradouro(String logradouro) {
+		this.logradouro = logradouro;
+	}
+	public String getNumero() {
+		return numero;
+	}
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+	public String getComplemento() {
+		return complemento;
+	}
+	public void setComplemento(String complemento) {
+		this.complemento = complemento;
+	}
+	public String getCep() {
+		return cep;
+	}
+	public void setCep(String cep) {
+		this.cep = cep;
+	}
+	public String getBairro() {
+		return bairro;
+	}
+	public void setBairro(String bairro) {
+		this.bairro = bairro;
+	}
+	public String getCidade() {
+		return cidade;
+	}
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+	public String getEstado() {
+		return estado;
+	}
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
 	private String numeroHabilitacao;
 	private String tipoHabilitacao;
 	private Date dataValidadeHabilitacao;
@@ -16,5 +102,14 @@ public class Segurado {
 	private String bairro;
 	private String cidade;
 	private String estado;
+	@Override
+	public String toString() {
+		return "Segurado [classe=" + classe + ", numeroRG=" + numeroRG + ", orgaoExpedidorRG=" + orgaoExpedidorRG
+				+ ", numeroHabilitacao=" + numeroHabilitacao + ", tipoHabilitacao=" + tipoHabilitacao
+				+ ", dataValidadeHabilitacao=" + dataValidadeHabilitacao + ", dataPrimeiraHabilitacao="
+				+ dataPrimeiraHabilitacao + ", logradouro=" + logradouro + ", numero=" + numero + ", complemento="
+				+ complemento + ", cep=" + cep + ", bairro=" + bairro + ", cidade=" + cidade + ", estado=" + estado
+				+ ", toString()=" + super.toString() + "]";
+	}
 	
 }
--- seguros/src/main/java/br/unibh/seguros/entidades/Setor.java
+++ seguros/src/main/java/br/unibh/seguros/entidades/Setor.java
@@ -8,6 +8,36 @@ public class Setor {
 	private String nome;
 	private String sigla;
 	private Setor setorSuperior;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getSigla() {
+		return sigla;
+	}
+	public void setSigla(String sigla) {
+		this.sigla = sigla;
+	}
+	public Setor getSetorSuperior() {
+		return setorSuperior;
+	}
+	public void setSetorSuperior(Setor setorSuperior) {
+		this.setorSuperior = setorSuperior;
+	}
+	public Set<Funcionario> getFuncionarios() {
+		return funcionarios;
+	}
+	public void setFuncionarios(Set<Funcionario> funcionarios) {
+		this.funcionarios = funcionarios;
+	}
 	private Set<Funcionario> funcionarios;
 
 }
