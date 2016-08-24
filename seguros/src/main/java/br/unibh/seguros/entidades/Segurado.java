dd408c41ff32e409c1e66f348ff271dd5485c939
11423066 <11423066@ESTB7LI0309.unibh.edu>
2016-08-24 20:08:37 -0300

    24.08.2016 passo 8 Completo

--- seguros/src/main/java/br/unibh/seguros/entidades/Segurado.java
+++ seguros/src/main/java/br/unibh/seguros/entidades/Segurado.java
@@ -4,9 +4,46 @@ import java.util.Date;
 
 public class Segurado extends Pessoa {
 
+	public Segurado(){}
+	
+	public Segurado(Long id, String nome, String sexo, String cpf, String telefoneComercial, String telefoneResidencial,
+			String telefoneCelular, String email, Date dataNascimento, Date dataCadastro, String classe,
+			String numeroRG, String orgaoExpedidorRG, String numeroHabilitacao, String tipoHabilitacao,
+			Date dataValidadeHabilitacao, Date dataPrimeiraHabilitacao, String logradouro, String numero,
+			String complemento, String cep, String bairro, String cidade, String estado) {
+		super(id, nome, sexo, cpf, telefoneComercial, telefoneResidencial, telefoneCelular, email, dataNascimento,
+				dataCadastro);
+		this.classe = classe;
+		this.numeroRG = numeroRG;
+		this.orgaoExpedidorRG = orgaoExpedidorRG;
+		this.numeroHabilitacao = numeroHabilitacao;
+		this.tipoHabilitacao = tipoHabilitacao;
+		this.dataValidadeHabilitacao = dataValidadeHabilitacao;
+		this.dataPrimeiraHabilitacao = dataPrimeiraHabilitacao;
+		this.logradouro = logradouro;
+		this.numero = numero;
+		this.complemento = complemento;
+		this.cep = cep;
+		this.bairro = bairro;
+		this.cidade = cidade;
+		this.estado = estado;
+	}
 	private String classe;
 	private String numeroRG;
 	private String orgaoExpedidorRG;
+	private String numeroHabilitacao;
+	private String tipoHabilitacao;
+	private Date dataValidadeHabilitacao;
+	private Date dataPrimeiraHabilitacao;
+	private String logradouro;
+	private String numero;
+	private String complemento;
+	private String cep;
+	private String bairro;
+	private String cidade;
+	private String estado;
+	
+	
 	public String getClasse() {
 		return classe;
 	}
@@ -91,17 +128,8 @@ public class Segurado extends Pessoa {
 	public void setEstado(String estado) {
 		this.estado = estado;
 	}
-	private String numeroHabilitacao;
-	private String tipoHabilitacao;
-	private Date dataValidadeHabilitacao;
-	private Date dataPrimeiraHabilitacao;
-	private String logradouro;
-	private String numero;
-	private String complemento;
-	private String cep;
-	private String bairro;
-	private String cidade;
-	private String estado;
+
+	
 	@Override
 	public String toString() {
 		return "Segurado [classe=" + classe + ", numeroRG=" + numeroRG + ", orgaoExpedidorRG=" + orgaoExpedidorRG
@@ -110,6 +138,107 @@ public class Segurado extends Pessoa {
 				+ dataPrimeiraHabilitacao + ", logradouro=" + logradouro + ", numero=" + numero + ", complemento="
 				+ complemento + ", cep=" + cep + ", bairro=" + bairro + ", cidade=" + cidade + ", estado=" + estado
 				+ ", toString()=" + super.toString() + "]";
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = super.hashCode();
+		result = prime * result + ((bairro == null) ? 0 : bairro.hashCode());
+		result = prime * result + ((cep == null) ? 0 : cep.hashCode());
+		result = prime * result + ((cidade == null) ? 0 : cidade.hashCode());
+		result = prime * result + ((classe == null) ? 0 : classe.hashCode());
+		result = prime * result + ((complemento == null) ? 0 : complemento.hashCode());
+		result = prime * result + ((dataPrimeiraHabilitacao == null) ? 0 : dataPrimeiraHabilitacao.hashCode());
+		result = prime * result + ((dataValidadeHabilitacao == null) ? 0 : dataValidadeHabilitacao.hashCode());
+		result = prime * result + ((estado == null) ? 0 : estado.hashCode());
+		result = prime * result + ((logradouro == null) ? 0 : logradouro.hashCode());
+		result = prime * result + ((numero == null) ? 0 : numero.hashCode());
+		result = prime * result + ((numeroHabilitacao == null) ? 0 : numeroHabilitacao.hashCode());
+		result = prime * result + ((numeroRG == null) ? 0 : numeroRG.hashCode());
+		result = prime * result + ((orgaoExpedidorRG == null) ? 0 : orgaoExpedidorRG.hashCode());
+		result = prime * result + ((tipoHabilitacao == null) ? 0 : tipoHabilitacao.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (!super.equals(obj))
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Segurado other = (Segurado) obj;
+		if (bairro == null) {
+			if (other.bairro != null)
+				return false;
+		} else if (!bairro.equals(other.bairro))
+			return false;
+		if (cep == null) {
+			if (other.cep != null)
+				return false;
+		} else if (!cep.equals(other.cep))
+			return false;
+		if (cidade == null) {
+			if (other.cidade != null)
+				return false;
+		} else if (!cidade.equals(other.cidade))
+			return false;
+		if (classe == null) {
+			if (other.classe != null)
+				return false;
+		} else if (!classe.equals(other.classe))
+			return false;
+		if (complemento == null) {
+			if (other.complemento != null)
+				return false;
+		} else if (!complemento.equals(other.complemento))
+			return false;
+		if (dataPrimeiraHabilitacao == null) {
+			if (other.dataPrimeiraHabilitacao != null)
+				return false;
+		} else if (!dataPrimeiraHabilitacao.equals(other.dataPrimeiraHabilitacao))
+			return false;
+		if (dataValidadeHabilitacao == null) {
+			if (other.dataValidadeHabilitacao != null)
+				return false;
+		} else if (!dataValidadeHabilitacao.equals(other.dataValidadeHabilitacao))
+			return false;
+		if (estado == null) {
+			if (other.estado != null)
+				return false;
+		} else if (!estado.equals(other.estado))
+			return false;
+		if (logradouro == null) {
+			if (other.logradouro != null)
+				return false;
+		} else if (!logradouro.equals(other.logradouro))
+			return false;
+		if (numero == null) {
+			if (other.numero != null)
+				return false;
+		} else if (!numero.equals(other.numero))
+			return false;
+		if (numeroHabilitacao == null) {
+			if (other.numeroHabilitacao != null)
+				return false;
+		} else if (!numeroHabilitacao.equals(other.numeroHabilitacao))
+			return false;
+		if (numeroRG == null) {
+			if (other.numeroRG != null)
+				return false;
+		} else if (!numeroRG.equals(other.numeroRG))
+			return false;
+		if (orgaoExpedidorRG == null) {
+			if (other.orgaoExpedidorRG != null)
+				return false;
+		} else if (!orgaoExpedidorRG.equals(other.orgaoExpedidorRG))
+			return false;
+		if (tipoHabilitacao == null) {
+			if (other.tipoHabilitacao != null)
+				return false;
+		} else if (!tipoHabilitacao.equals(other.tipoHabilitacao))
+			return false;
+		return true;
 	}
 	
 }
--- seguros/src/main/java/br/unibh/seguros/entidades/Setor.java
+++ seguros/src/main/java/br/unibh/seguros/entidades/Setor.java
@@ -4,6 +4,14 @@ import java.util.Set;
 
 public class Setor {
 	
+	public Setor(Long id, String nome, String sigla, Setor setorSuperior, Set<Funcionario> funcionarios) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.sigla = sigla;
+		this.setorSuperior = setorSuperior;
+		this.funcionarios = funcionarios;
+	}
 	private Long id;
 	private String nome;
 	private String sigla;
@@ -39,6 +47,58 @@ public class Setor {
 		this.funcionarios = funcionarios;
 	}
 	private Set<Funcionario> funcionarios;
+	@Override
+	public String toString() {
+		return "Setor [id=" + id + ", nome=" + nome + ", sigla=" + sigla + ", setorSuperior=" + setorSuperior
+				+ ", funcionarios=" + funcionarios + "]";
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((funcionarios == null) ? 0 : funcionarios.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+		result = prime * result + ((setorSuperior == null) ? 0 : setorSuperior.hashCode());
+		result = prime * result + ((sigla == null) ? 0 : sigla.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Setor other = (Setor) obj;
+		if (funcionarios == null) {
+			if (other.funcionarios != null)
+				return false;
+		} else if (!funcionarios.equals(other.funcionarios))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (nome == null) {
+			if (other.nome != null)
+				return false;
+		} else if (!nome.equals(other.nome))
+			return false;
+		if (setorSuperior == null) {
+			if (other.setorSuperior != null)
+				return false;
+		} else if (!setorSuperior.equals(other.setorSuperior))
+			return false;
+		if (sigla == null) {
+			if (other.sigla != null)
+				return false;
+		} else if (!sigla.equals(other.sigla))
+			return false;
+		return true;
+	}
 
 }
 
