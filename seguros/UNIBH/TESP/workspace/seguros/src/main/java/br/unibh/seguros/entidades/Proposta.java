a847a4dd82ca1d7ed795447510aabd2173b76065
11423066 <11423066@ESTB7LI0304.unibh.edu>
2016-09-28 20:41:58 -0300

    asd

--- seguros/UNIBH/TESP/workspace/seguros/src/main/java/br/unibh/seguros/entidades/Proposta.java
+++ seguros/UNIBH/TESP/workspace/seguros/src/main/java/br/unibh/seguros/entidades/Proposta.java
@@ -16,7 +16,6 @@ import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
-import javax.persistence.UniqueConstraint;
 import javax.persistence.Version;
 
 
