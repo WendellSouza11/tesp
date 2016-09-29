1188894f066b116c48237d07fc7dda5639de0eaf
11423066 <11423066@ESTB7LI0304.unibh.edu>
2016-09-29 19:57:46 -0300

    asdasdasdasdasd

--- seguros/UNIBH/TESP/workspace/seguros/src/test/java/seguros/Teste.java
+++ seguros/UNIBH/TESP/workspace/seguros/src/test/java/seguros/Teste.java
@@ -9,72 +9,72 @@ import br.unibh.seguros.entidades.Setor;
 
 public class Teste {
 
-	
 	@Test
-	public void testeCriacaoFuncionario(){ 
-		
-			Funcionario o = new Funcionario (1L,"Maria","F","0123456789",null, null, null, null, null, null, null, null, null);
-			System.out.println(o);
-			Assert.assertNotNull(o);		
-		
+	public void testeCriacaoFuncionario() {
+
+		Funcionario o = new Funcionario(1L, "Maria", "F", "0123456789", null, null, null, null, null, null, null, null,
+				null);
+		System.out.println(o);
+		Assert.assertNotNull(o);
+
 	}
-	
-	@Test 
-	public void testeComparacaoFuncionario(){
-		
-		
-		Funcionario o = new Funcionario (1L,"Joao", "M", "0123456789", null, null, null, null, null, null, null, null, null);
-		Funcionario o2 = new Funcionario (1L, "Joao", "M", "0123456789", null, null, null, null, null, null, null, null, null);
+
+	@Test
+	public void testeComparacaoFuncionario() {
+
+		Funcionario o = new Funcionario(1L, "Joao", "M", "0123456789", null, null, null, null, null, null, null, null,
+				null);
+		Funcionario o2 = new Funcionario(1L, "Joao", "M", "0123456789", null, null, null, null, null, null, null, null,
+				null);
 		Assert.assertTrue(o.equals(o2));
-		Funcionario o3 = new Funcionario (2L,"Maria","F", "0123456789", null, null, null, null,null,null, null, null, null);
+		Funcionario o3 = new Funcionario(2L, "Maria", "F", "0123456789", null, null, null, null, null, null, null, null,
+				null);
 		Assert.assertFalse(o.equals(o3));
-		
-	}
-		
-		@Test
-		public void testeCriacaoSegurado(){
-			
-				Segurado o = new Segurado(1L,"João","M","11111111111",null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null);
-				System.out.println(o);
-				Assert.assertNotNull(o);		
-			
-		}
-		
-		@Test 
-		public void testeComparacaoSegurado(){
-			
-			
-			Segurado o = new Segurado (1L,"Joao", "M", "11111111111", null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null);
-			Segurado o2 = new Segurado (1L,"Joao","M", "11111111111", null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null);
-			Assert.assertTrue(o.equals(o2));
-			Segurado o3 = new Segurado (2L,"Maria","F","11111111111",null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null);
-			Assert.assertFalse(o.equals(o3));
-				
-			
-		}	
 
-			@Test
-			public void testeCriacaoSetor(){
-				
-					Setor o = new Setor(1L,"Recursos Humanos","RH");
-					System.out.println(o);
-					Assert.assertNotNull(o);		
-				
-			}
-			
-			@Test 
-			public void testeComparacaoSetor(){
-				
-				
-				Setor o = new Setor (1L,"Recursos Humanos", "RH");
-				Setor o2 = new Setor (1L,"Recursos Humanos","RH");
-				Assert.assertTrue(o.equals(o2));
-				Setor o3 = new Setor (2L,"Operações","op");
-				Assert.assertFalse(o.equals(o3));
-				
-						
 	}
-	
+
+	@Test
+	public void testeCriacaoSegurado() {
+
+		Segurado o = new Segurado(1L, "João", "M", "11111111111", null, null, null, null, null, null, null, null, null,
+				null, null, null, null, null, null, null, null, null, null, null);
+		System.out.println(o);
+		Assert.assertNotNull(o);
+
+	}
+
+	@Test
+	public void testeComparacaoSegurado() {
+
+		Segurado o = new Segurado(1L, "Joao", "M", "11111111111", null, null, null, null, null, null, null, null, null,
+				null, null, null, null, null, null, null, null, null, null, null);
+		Segurado o2 = new Segurado(1L, "Joao", "M", "11111111111", null, null, null, null, null, null, null, null, null,
+				null, null, null, null, null, null, null, null, null, null, null);
+		Assert.assertTrue(o.equals(o2));
+		Segurado o3 = new Segurado(2L, "Maria", "F", "11111111111", null, null, null, null, null, null, null, null,
+				null, null, null, null, null, null, null, null, null, null, null, null);
+		Assert.assertFalse(o.equals(o3));
+
+	}
+
+	@Test
+	public void testeCriacaoSetor() {
+
+		Setor o = new Setor(1L, "Recursos Humanos", "RH");
+		System.out.println(o);
+		Assert.assertNotNull(o);
+
+	}
+
+	@Test
+	public void testeComparacaoSetor() {
+
+		Setor o = new Setor(1L, "Recursos Humanos", "RH");
+		Setor o2 = new Setor(1L, "Recursos Humanos", "RH");
+		Assert.assertTrue(o.equals(o2));
+		Setor o3 = new Setor(2L, "Operações", "op");
+		Assert.assertFalse(o.equals(o3));
+
+	}
+
 }
-
-
